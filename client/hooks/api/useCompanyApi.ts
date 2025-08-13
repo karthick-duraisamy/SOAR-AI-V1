382fb5e2fd0cf93549542068f4a850883e8771c5
karthick-duraisamy <karthi17897@gmail.com>
2025-08-13 18:50:52 +0530

    Api_end_point changed in Hooks

--- client/hooks/api/useCompanyApi.ts
+++ client/hooks/api/useCompanyApi.ts
@@ -2,7 +2,7 @@ import { useState, useCallback } from 'react';
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 // const API_BASE_URL = '/api';
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 interface ApiState<T> {
   data: T | null;
